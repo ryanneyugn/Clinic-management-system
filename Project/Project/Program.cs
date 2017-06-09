7822a0488c8f99e0d0c4eb07f3e06254a1e92350
tmtshun <tmthuan95@gmail.com>
2017-06-09 13:43:11 +0700

    add Project

---
+++ Project/Project/Program.cs
@@ -0,0 +1,20 @@
+﻿using Project.GIAODIEN;
+using System;
+using System.Windows.Forms;
+
+namespace Project
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new gd_nhanbenh());
+        }
+    }
+}
