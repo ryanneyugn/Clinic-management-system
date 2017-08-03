9359c981ac4f9c75015c50741ce64301cc3bd0ba
tmtshun <tmtvvinner@outlook.com>
2017-08-03 17:58:59 +0700

    3/8 5:58
    code frm Setting

---
+++ Project/NHANBENH/Setting.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Project
+{
+    partial class Setting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServerIP = new System.Windows.Forms.TextBox();
+            this.txtClient_1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtClient_2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server IP:";
+            // 
+            // txtServerIP
+            // 
+            this.txtServerIP.Location = new System.Drawing.Point(83, 30);
+            this.txtServerIP.Name = "txtServerIP";
+            this.txtServerIP.Size = new System.Drawing.Size(100, 20);
+            this.txtServerIP.TabIndex = 1;
+            // 
+            // txtClient_1
+            // 
+            this.txtClient_1.Location = new System.Drawing.Point(83, 71);
+            this.txtClient_1.Name = "txtClient_1";
+            this.txtClient_1.Size = new System.Drawing.Size(100, 20);
+            this.txtClient_1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Client_1 IP:";
+            // 
+            // txtClient_2
+            // 
+            this.txtClient_2.Location = new System.Drawing.Point(83, 117);
+            this.txtClient_2.Name = "txtClient_2";
+            this.txtClient_2.Size = new System.Drawing.Size(100, 20);
+            this.txtClient_2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Client_2 IP:";
+            // 
+            // Setting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtClient_2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtClient_1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServerIP);
+            this.Controls.Add(this.label1);
+            this.Name = "Setting";
+            this.Text = "Setting";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServerIP;
+        private System.Windows.Forms.TextBox txtClient_1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtClient_2;
+        private System.Windows.Forms.Label label3;
+    }
+}
